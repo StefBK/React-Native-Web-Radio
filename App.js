91b8ed3c8a0a35ed98aede9f6f16a6074b1fc3b7
Stef BK <stefsbk@gmail.com>
2022-05-05 13:41:51 +0200

    Adding Contact Page - Switch on Both Pages with Home - Adding a Radio

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as Linking from 'expo-linking';
 import { TouchableOpacity } from 'react-native-web';
+import { Audio } from 'expo-av';
+import { useState, useEffect } from 'react';
+import { useFonts,Comfortaa_400Regular} from '@expo-google-fonts/comfortaa'; 
+import AppLoading from 'expo-app-loading';
 
-const homeLink=()=>{
-  Linking.openURL('https://google.com');
-}
+const AUDIO_STREAM = "http://direct.radioarmenie.com:9029/stream?type=http&nocache=197416"; 
 
 const callLink=()=>{
   Linking.openURL('tel:0680562492');
@@ -16,7 +18,47 @@ const mailLink=()=>{
   Linking.openURL('mailto:stefsbk@gmail.com');
 }
 
+const Contact=()=>{
+  return(
+    <View style={styles.content}>
+      <Text>Page Contact</Text>
+    </View>
+  )
+}
+
 export default function App() {
+
+  const [page, setPage]=useState('Home');
+
+  const [sound, setSound]=useState(null);
+
+  const[fontLoaded]=useFonts({
+    Comfortaa_400Regular
+  });
+  if (!fontLoaded){
+    return <AppLoading/>;
+  }
+
+  const homeLink=()=>{
+    // Linking.openURL('https://google.com');
+    setPage('Home');
+  }
+
+  async function playSound(){
+    if (sound===null){
+      const {sound}=await Audio.Sound.createAsync(
+        {uri: AUDIO_STREAM}
+      )
+      setSound(sound);
+      sound.playAsync();
+      console.log(sound);
+    }else{
+      setSound(null);
+      sound.stopAsync();
+      console.log(sound);
+    }
+  }
+
   return (
     <View style={styles.container}>
 
@@ -24,20 +66,45 @@ export default function App() {
         <TouchableOpacity onPress={homeLink}>
           <Ionicons name="home" size={24} color="black" />
         </TouchableOpacity>
-        <Text>WEB RADIO</Text>
+        <Text style={{
+          color:'orange',
+          fontFamily:'Comfortaa_400Regular',
+          size:40
+        }}>RADIO ARMÉNIE</Text>
         <Ionicons name="person" size={24} color="black" />
       </View>
-      
-      <View style={styles.content}>
-        <Text>CONTENT</Text>
-      </View>
+
+      {
+        (()=>{
+          switch(page){
+            case 'Contact':
+              return <Contact/>
+            case 'Home':
+              console.log('Switch Case Home');
+              return (
+                <ImageBackground source={require("./assets/arm.jpg")} style={styles.content}>
+                <TouchableOpacity onPress={()=>{playSound()}}>
+                  <Ionicons name={
+                    sound===null? "play-circle":"pause-circle"
+                  }
+                  size={240} color="white" />
+                </TouchableOpacity>
+              </ImageBackground>
+              )
+          }
+        })()
+      }
       
       <View style={styles.footer}>
         <TouchableOpacity onPress={callLink}>
           <Ionicons name="call" size={24} color="black" />
         </TouchableOpacity>
         <Ionicons name="logo-instagram" size={24} color="black" />
-        <TouchableOpacity onPress={mailLink}>
+        <TouchableOpacity onPress={
+          ()=>{
+            setPage('Contact');
+            }
+          }>
           <Ionicons name="mail" size={24} color="black" />
         </TouchableOpacity>
       </View>
@@ -77,6 +144,8 @@ const styles = StyleSheet.create({
   content:{
     backgroundColor:'blue',
     flex:5,
-    padding:20
+    padding:20,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
